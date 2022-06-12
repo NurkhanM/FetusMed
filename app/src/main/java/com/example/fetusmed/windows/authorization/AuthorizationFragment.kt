30338bde66d67ba400775ac75aeb319a8d01cd16
NurkhanM <MXan.2000@gmail.com>
2022-06-12 16:00:49 +0600

    1 windows/authorization

---
+++ app/src/main/java/com/example/fetusmed/windows/authorization/AuthorizationFragment.kt
@@ -0,0 +1,25 @@
+package com.example.fetusmed.windows.authorization
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fetusmed.R
+
+class AuthorizationFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_authorization, container, false)
+
+
+        return view
+    }
+
+
+}
---
+++ app/src/main/java/com/example/fetusmed/windows/splash/SplashFragment.kt
@@ -0,0 +1,54 @@
+package com.example.fetusmed.windows.splash
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import com.example.fetusmed.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_splash, container, false)
+
+        // Метод для показа полно экранного режима
+        fullScreen()
+
+        // Таймер для показа Рекламы
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_authorizationFragment)
+        }
+
+        return view
+
+    }
+
+    @SuppressLint("ObsoleteSdkInt")
+    fun fullScreen() {
+
+        val uiOptions = (activity as AppCompatActivity).window.decorView.systemUiVisibility
+        var newUiOptions = uiOptions
+
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
+        (activity as AppCompatActivity).window.decorView.systemUiVisibility = newUiOptions
+    }
+
+
+}
